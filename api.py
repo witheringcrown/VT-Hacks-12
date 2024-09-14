e59fb211359c1e75fa01523615eefdf34d22e80a
Dakota Vaughan <witheringcrown@gmail.com>
2024-09-14 16:48:05 -0400

    python react connected
    I don't actually really know how this works but it's kinda set up? We'll need to figure out how to actually make new functions and connect them, tho

--- api.py
+++
@@ -1,7 +0,0 @@
-from Flask import flask
-
-app = Flask(__name__)
-
-@app.route('/time')
-def get_time():
-    return {'time': time.time()}
--- dnd-character-generator/api/api.py
+++ dnd-character-generator/api/api.py
@@ -4,6 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/time')
-
 def get_time():
     return {'time': time.time()}
+
+@app.route('/hello_world')
+def hello_world():
+    return 7
