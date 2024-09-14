3abe28a5e3305a3b1a847cc908628813f2be3066
Dakota Vaughan <witheringcrown@gmail.com>
2024-09-14 15:21:36 -0400

    React Flask Setup

--- main.py
+++
@@ -1,9 +0,0 @@
-from website import create_app
-
-app = create_app()
-
-# makes so the web server is run only
-# from this file directly
-if __name__ == '__main__':
-    app.run(debug=True) # debug mode, turn off later
-
--- website/views.py
+++
@@ -1,8 +0,0 @@
-# stores the screens/views of the website
-from flask import Blueprint
-
-views = Blueprint('views', __name__)
-
-@views.route('/')
-def home():
-    return "<h1>Hello World</h1>"
