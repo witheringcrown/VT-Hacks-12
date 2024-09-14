e95f9671060fe69d5749b289cc6596521b91f4df
Juren D. <jake@getdonovan.com>
2024-09-14 18:24:35 -0400

    added pages on api.py to handle login added dbtest.html for temp development

--- dnd-character-generator/api/api.py
+++ dnd-character-generator/api/api.py
@@ -1,8 +1,13 @@
 import time
-from flask import Flask
+from time import sleep
+from flask import Flask, render_template, redirect, request, session, url_for
 
 app = Flask(__name__)
+app.secret_key = "umm_the_desk_is"
 
+@app.route('/')
+def home():
+    return("hi")
 @app.route('/time')
 def get_time():
     return {'time': time.time()}
@@ -10,3 +15,37 @@ def get_time():
 @app.route('/hello_world')
 def hello_world():
     return 7
+    
+    
+    
+
+
+ 
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method =="POST":
+        user = request.form["name"]
+        session["user"] = user
+        return redirect(url_for("mypage"))
+    else:
+        return render_template("dbtest.html")
+
+@app.route("/mypage")
+def mypage():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>hi, {user}. this is editable<h/1>"
+    else:
+        return redirect(url_for("login"))
+        
+        
+        
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+    
+    
+if __name__ == "__main__":
+    app.run(debug=True)
