3ed10b1578c4108844320daeedbdb063415e332f
Juren D. <jake@getdonovan.com>
2024-09-15 00:07:13 -0400

    merg shit, add db other work

--- dnd-character-generator/api/api.py
+++ dnd-character-generator/api/api.py
@@ -1,9 +1,64 @@
 import time
+import os
 from time import sleep
 from flask import Flask, render_template, redirect, request, session, url_for
+from flask_sqlalchemy import SQLAlchemy
+from propelauth_flask import init_auth
+
 
 app = Flask(__name__)
 app.secret_key = "umm_the_desk_is"
+file_path_users = os.path.abspath(os.getcwd())+"/db/users.db"
+
+################################## DB stuff ####################################
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///"+file_path_users
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+
+
+
+
+class users(db.Model):
+    _id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32))
+    email = db.Column(db.String(320))
+    loginonly = db.Column(db.Boolean)
+    
+    def __init__(self, _id, name, email, loginonly):
+        self._id = _id
+        self.name = name 
+        self.email = email
+        self.loginonly = loginonly
+        
+with app.app_context():
+    db.create_all()
+
+############################## auth stuff #####################
+
+auth = init_auth(
+    "https://93765821396.propelauthtest.com",
+    "c1879b86355b06b8cb74f54077536b38d31da87b2d53465ae18fa86fad78c31e510eb575f920a8c9f52051f534c8b015"
+    )
+
+
+
+
+
+
+
+
+
+
+#######################backend stuff #######################
+
+
+
+
+
+
+
 
 @app.route('/')
 def home():
@@ -19,9 +74,6 @@ def hello_world():
     
     
 
-
- 
-
 @app.route('/login', methods=["POST", "GET"])
 def login():
     if request.method =="POST":
@@ -32,6 +84,7 @@ def login():
         return render_template("dbtest.html")
 
 @app.route("/mypage")
+@auth.require_user
 def mypage():
     if "user" in session:
         user = session["user"]
@@ -46,6 +99,21 @@ def logout():
     session.pop("user", None)
     return redirect(url_for("login"))
     
+    
+@app.route("/user/<username>")
+def userpage(username):
+    if "user" in session and username == session["user"]:
+        return redirect(url_for("mypage"))
+     # add in auth function
+    else:
+        userpage_private = db.session.execute(db.select(users.loginonly).where(users.name == username)).scalar()
+        userpage_email = db.session.execute(db.select(users.email).where(users.name == username)).scalar()
+        return {"privacy":userpage_private,
+                "email":userpage_email}
+        
+        
+        
+            
     
 if __name__ == "__main__":
     app.run(debug=True)
--- dnd-character-generator/api/db.py
+++ dnd-character-generator/api/db.py
@@ -1,13 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3"
+import api.py
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = flase
 
 db = SQLAlchemy(app)
+app = Flask(__name__)
 
 class users(db.Model):
     _id = db.Column("id", db.Interger, pirmary_key=True)
     name = db.Column(db.String(32))
-    email = db.Column(db.String(320)
-    passw = db.Column(db.String(32))
+    email = db.Column(db.String(320))
+    test = db.Column(db.String(32))
+    loginonly = db.Column(db.Bool)
     
-    def __init__(self, name, passw)
+    def __init__(self, name, test):
+        self.name = name
+        self.email = email
+    
+with app.ap_context():
+    db.create_all()
